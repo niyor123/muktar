0db782ea8c5fa591d5e2ff971bba823ce21902ee
Niyor Boruah <90924760+niyor123@users.noreply.github.com>
2022-11-05 14:46:29 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,29 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "App Developer", "Web Developer", "UI/UX Designer", "Data Analysis"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
+
+
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+
